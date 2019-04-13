cbe6d7d3fbae679acbba855b0c9fb7bde4d6be32
jefferson <jeffersonl875@outlook.com>
2019-04-13 17:04:24 -0300

    primeiro envio

---
+++ calc.js
@@ -0,0 +1,6 @@
+var somaFunc = require('./calcula/soma');
+var divideFunc = require('./calcula/divide');
+
+console.log(somaFunc(1,8));
+console.log(divideFunc(2,2));
+console.log(somaFunc(1,1) * divideFunc(12,2));
